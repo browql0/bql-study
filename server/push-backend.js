e958940740b6f1823aa39a942a313fae36a8ada1
browql0 <alihajjaj930@gmail.com>
2025-12-01 23:24:03 +0100

    ajout de railway

--- server/push-backend.js
+++ server/push-backend.js
@@ -14,7 +14,7 @@ const VAPID_PUBLIC_KEY = process.env.VAPID_PUBLIC_KEY;
 const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY;
 
 webpush.setVapidDetails(
-  'mailto:admin@tonsite.com',
+  'mailto:alihajjaj930@gmail.com',
   VAPID_PUBLIC_KEY,
   VAPID_PRIVATE_KEY
 );
